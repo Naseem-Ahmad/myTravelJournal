5773a6b9ccfb5a9193b71e192ef931eb7304e7ae
Naseem Ahmad <naseemahmad1998@gmail.com>
2025-10-22 22:48:47 +0530

    Resolving deployment issue

---
+++ data.js
@@ -0,0 +1,39 @@
+export default [
+        {
+        id: 1,
+        img: {
+            src: "./images/LondonBridge.jpg",
+            alt: "London Bridge"
+        },
+        title: "London Bridge",
+        country: "England",
+        googleMapsLink: "https://www.google.com/maps/place/London+Bridge/@51.5102072,-0.0913736,16.64z/data=!4m14!1m7!3m6!1s0x487603515c8e79b7:0xd641ab4af97ceb75!2sBCLP+-+Bryan+Cave+Leighton+Paisner!8m2!3d51.5104948!4d-0.0889826!16s%2Fg%2F1v62h3x2!3m5!1s0x4876035159bb13c5:0xa61e28267c3563ac!8m2!3d51.5078788!4d-0.0877321!16zL20vMHA3N2c?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D",
+        dates: "May , 2024",
+        text: "The London Bridge is a historic river crossing over the River Thames, connecting central London to Southwark. The current bridge opened in 1973, replacing several older versions dating back to Roman times. Often confused with the more decorative Tower Bridge, London Bridge is simpler in design but holds great historical and strategic importance. It remains a key route for both vehicles and pedestrians in the city."
+    },
+    {
+        id: 2, 
+        img: {
+            src: "./images/LondonEye.jpg",
+            alt: "London Eye"
+        },
+        title: "London Eye",
+        country: "England",
+        googleMapsLink: "https://www.google.com/maps/place/London+Eye/@51.5031897,-0.1220941,17z/data=!3m2!4b1!5s0x487604c79a2ef535:0x6b1752373d1ab417!4m6!3m5!1s0x487604b900d26973:0x4291f3172409ea92!8m2!3d51.5031864!4d-0.1195192!16zL20vMDdneWM?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D",
+        dates: "May , 2024",
+        text: "The London Eye is a giant observation wheel on the South Bank of the River Thames. Standing at 135 meters, it offers stunning views of London through 32 glass capsules. Opened in 2000, it’s one of the city’s top tourist attractions and a modern symbol of London."
+    },
+    
+    {
+        id: 3,
+        img: {
+            src: "./images/BigBen2.jpg",
+            alt: "Big Ben"
+        },
+        title: "Big Ben",
+        country: "England",
+        googleMapsLink: "https://www.google.com/maps/place/Big+Ben/@51.5016878,-0.1223385,16.85z/data=!3m1!5s0x47d8e55e300273ad:0x64ade3f4995c75cd!4m14!1m7!3m6!1s0x487604b900d26973:0x4291f3172409ea92!2sLondon+Eye!8m2!3d51.5031864!4d-0.1195192!16zL20vMDdneWM!3m5!1s0x487604c38c8cd1d9:0xb78f2474b9a45aa9!8m2!3d51.5007292!4d-0.1246254!16zL20vMDI2eWRj?entry=ttu&g_ep=EgoyMDI1MTAxNC4wIKXMDSoASAFQAw%3D%3D",
+        dates: "May , 2024",
+        text: "Big Ben is one of the most famous landmarks in the world and a symbol of British identity. Located at the north end of the Palace of Westminster in London, it has become a national emblem — often used in films, postcards, and news to represent London or the UK as a whole. Tourists from around the globe visit the site to see this architectural marvel."
+    },
+]
